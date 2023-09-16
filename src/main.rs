013cfd07a66316b351ad804332400d65f26e0a6a
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2023-09-16 21:29:29 +0100

    Playing with Hyper's examples

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,38 @@
-fn main() {
+use anyhow::Error;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+async fn handle_request(request: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+
+    match (request.method(), request.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from("Try posting data to /echo");
+        }
+
+        (&Method::POST, "/echo") => {
+            *response.body_mut() = Body::from("TODOOOOOOOOOOOO");
+        }
+
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    }
+
+    Ok(response)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let addr = SocketAddr::from(([0, 0, 0, 0], 10001));
+    let make_svc =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_request)) });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    server.await?;
+
+    Ok(())
 }
