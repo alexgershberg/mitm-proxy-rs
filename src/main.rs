3e6a205fcbb0a51038e688b0b6683f5b4eb3d310
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2023-09-16 22:16:25 +0100

    Very simple http proxy

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
 use anyhow::Error;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::{Body, Client, Request, Response, Server, StatusCode};
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
 async fn handle_request(request: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let mut response = Response::new(Body::empty());
+    // let mut response = Response::new(Body::empty());
 
-    match (request.method(), request.uri().path()) {
-        (&Method::GET, "/") => {
-            *response.body_mut() = Body::from("Try posting data to /echo");
-        }
+    // match (request.method(), request.uri().path()) {
+    //     (&Method::GET, "/") => {
+    //         *response.body_mut() = Body::from("Try posting data to /echo");
+    //     }
+    //
+    //     (&Method::POST, "/echo") => *response.body_mut() = request.into_body(),
+    //
+    //     _ => {
+    //         *response.status_mut() = StatusCode::NOT_FOUND;
+    //     }
+    // }
 
-        (&Method::POST, "/echo") => {
-            *response.body_mut() = Body::from("TODOOOOOOOOOOOO");
-        }
+    // println!("{request:?}");
+    // if let Some(host) = request.headers().get("host") {
+    //     let client = Client::new();
+    //     client.request(request).await.unwrap();
+    // } else {
+    //     eprintln!("Host not found?");
+    // }
 
-        _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
+    println!("{request:?}");
+    let client = Client::new();
+    let response = match client.request(request).await {
+        Ok(resp) => resp,
+        Err(err) => {
+            eprintln!("Can't handle this one (YET!) | Error: {err}");
+            Response::builder()
+                .status(StatusCode::NOT_IMPLEMENTED)
+                .body(Body::empty())
+                .unwrap()
         }
-    }
+    };
 
     Ok(response)
 }
