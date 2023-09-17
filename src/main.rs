1787669b9649c6c57b4139020002032ae92e4173
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2023-09-17 13:38:15 +0100

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use anyhow::Error;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Client, Request, Response, Server, StatusCode};
 use hyper_rustls::{ConfigBuilderExt, HttpsConnectorBuilder};
+use lazy_static::lazy_static;
 use rustls::{Certificate, ClientConfig, PrivateKey};
 use std::convert::Infallible;
 use std::fs::File;
 use std::io::BufReader;
 use std::net::SocketAddr;
-use std::sync::{Arc};
-use lazy_static::lazy_static;
+use std::sync::Arc;
 use tokio::sync::Mutex;
 
 // https://docs.rs/rustls/latest/rustls/struct.Certificate.html
@@ -34,7 +34,6 @@ fn load_private_key_from_file(path: &str) -> Result<PrivateKey, Box<dyn std::err
 }
 
 async fn handle_request(mut request: Request<Body>) -> Result<Response<Body>, Infallible> {
-
     let mut count_locked = counter.lock().await;
     *count_locked += 1;
 
@@ -74,22 +73,18 @@ async fn handle_request(mut request: Request<Body>) -> Result<Response<Body>, In
 }
 
 lazy_static! {
-    static ref counter: Arc<Mutex<u64>>= Arc::new(Mutex::new(0));
+    static ref counter: Arc<Mutex<u64>> = Arc::new(Mutex::new(0));
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 10001));
 
-    let make_svc =
-        make_service_fn(|_conn| async {
-            Ok::<_, Infallible>(
-                service_fn(|req| handle_request(req))
-            )
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(|req| handle_request(req)))
+    });
 
-        });
-
-    let  server = Server::bind(&addr).serve(make_svc);
+    let server = Server::bind(&addr).serve(make_svc);
 
     server.await?;
 
