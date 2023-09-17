4ffd0b9844d0c53608fe44a31112ad1931375078
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2023-09-17 10:53:49 +0100

    Random stumblings

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,47 @@
-use anyhow::Error;
+use anyhow::{Error};
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Client, Request, Response, Server, StatusCode};
 use hyper_rustls::HttpsConnectorBuilder;
-use rustls::{ClientConfig, RootCertStore};
+use rustls::{Certificate, ClientConfig, PrivateKey, RootCertStore};
 use std::convert::Infallible;
 use std::fs;
+use std::fs::File;
 use std::io::BufReader;
 use std::net::SocketAddr;
 
+// https://docs.rs/rustls/latest/rustls/struct.Certificate.html
+fn load_certificates_from_file(path: &str) -> std::io::Result<Vec<Certificate>> {
+    let file = File::open(path)?;
+    let mut reader = BufReader::new(file);
+    let certs = rustls_pemfile::certs(&mut reader)?;
+
+    Ok(certs.into_iter().map(Certificate).collect())
+}
+
+// https://docs.rs/rustls/latest/rustls/struct.PrivateKey.html
+fn load_private_key_from_file(path: &str) -> Result<PrivateKey, Box<dyn std::error::Error>> {
+    let file = File::open(&path)?;
+    let mut reader = BufReader::new(file);
+    let mut keys = rustls_pemfile::pkcs8_private_keys(&mut reader)?;
+
+    match keys.len() {
+        0 => Err(format!("No PKCS8-encoded private key found in {path}").into()),
+        1 => Ok(PrivateKey(keys.remove(0))),
+        _ => Err(format!("More than one PKCS8-encoded private key found in {path}").into()),
+    }
+}
 async fn handle_request(request: Request<Body>) -> Result<Response<Body>, Infallible> {
     println!("{request:?}");
 
-    let f = fs::File::open("certs/sample.pem").unwrap();
-    let mut rd = BufReader::new(f);
-    let certs = rustls_pemfile::certs(&mut rd).map_err(|e| eprintln!("Failed to read certificate: {e}")).unwrap();
+    let cer_file = fs::File::open("certs/mitm-proxy-rs.cer").unwrap();
+    let mut cer_rd = BufReader::new(cer_file);
+    let certs = rustls_pemfile::certs(&mut cer_rd).map_err(|e| eprintln!("Failed to read certificate: {e}")).unwrap();
+
+
+
+    let cer = load_certificates_from_file("certs/mitm-proxy-rs.cer").unwrap();
+    let pkey = load_private_key_from_file("certs/mitm-proxy-rs.key").unwrap();
+
 
     // heavy "inspiration" taken from here: https://github.com/rustls/hyper-rustls/blob/main/examples/client.rs
     let mut roots = RootCertStore::empty();
@@ -22,7 +50,8 @@ async fn handle_request(request: Request<Body>) -> Result<Response<Body>, Infall
     let tls = ClientConfig::builder()
         .with_safe_defaults()
         .with_root_certificates(roots)
-        .with_no_client_auth();
+        .with_client_auth_cert(cer, pkey)
+        .unwrap();
 
     let https = HttpsConnectorBuilder::new()
         .with_tls_config(tls)
