112bfcba1e49073f2453c99e2f2beb80ed07f284
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2023-09-17 13:37:44 +0100

    Counter for debugging hyper

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use std::convert::Infallible;
 use std::fs::File;
 use std::io::BufReader;
 use std::net::SocketAddr;
+use std::sync::{Arc};
+use lazy_static::lazy_static;
+use tokio::sync::Mutex;
 
 // https://docs.rs/rustls/latest/rustls/struct.Certificate.html
 fn load_certificates_from_file(path: &str) -> std::io::Result<Vec<Certificate>> {
@@ -29,7 +32,13 @@ fn load_private_key_from_file(path: &str) -> Result<PrivateKey, Box<dyn std::err
         _ => Err(format!("More than one PKCS8-encoded private key found in {path}").into()),
     }
 }
+
 async fn handle_request(mut request: Request<Body>) -> Result<Response<Body>, Infallible> {
+
+    let mut count_locked = counter.lock().await;
+    *count_locked += 1;
+
+    println!("Counter: {count_locked}");
     println!("{request:?}");
 
     let cer = load_certificates_from_file("certs/mitm-proxy-rs.cer").unwrap();
@@ -64,13 +73,23 @@ async fn handle_request(mut request: Request<Body>) -> Result<Response<Body>, In
     Ok(response)
 }
 
+lazy_static! {
+    static ref counter: Arc<Mutex<u64>>= Arc::new(Mutex::new(0));
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 10001));
-    let make_svc =
-        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_request)) });
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let make_svc =
+        make_service_fn(|_conn| async {
+            Ok::<_, Infallible>(
+                service_fn(|req| handle_request(req))
+            )
+
+        });
+
+    let  server = Server::bind(&addr).serve(make_svc);
 
     server.await?;
 
